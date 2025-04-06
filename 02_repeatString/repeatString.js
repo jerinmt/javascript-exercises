630dca2140c96dae4ac6121a15e92b18bea9c1cb
jerinmt <jerinmt379@gmail.com>
2025-04-06 18:30:03 +0530

    Finished 3 exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    if(num<0) {
+        return 'ERROR';
+    }
+    let finalString = '';
+    for(let i=0;i<num;i++) {
+        finalString += str;
+    }
+    return finalString;
 };
-
+/*repeatString('hey', 3);
+repeatString('hello',10);
+repeatString('hi', 1);
+repeatString('bye', 0);*/
 // Do not edit below this line
 module.exports = repeatString;
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    const tempArray = [];
+    let finalString = '';
+    for(let i=0;i<str.length;i++) {
+        tempArray[i] = str[i];
+    }
+    for(let i=str.length-1;i>=0;i--) {
+        finalString += tempArray[i];
+    }
+    return finalString;
 };
 
 // Do not edit below this line
